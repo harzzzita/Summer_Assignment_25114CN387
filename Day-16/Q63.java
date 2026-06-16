1882154ad7dfbc818d4cf0e66565c1d874ed69bf
HARSHITA BISHT <25114cn387@glbitm.ac.in>
2026-06-16 16:47:17 +0530

    Added day 16 program

---
+++ Day-16/Q63.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Q63 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter target sum: ");
+        int target = sc.nextInt();
+        System.out.println("Pairs are:");
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                }
+            }
+        }
+    }
+}
